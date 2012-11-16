cd89f612d6b81dc446819940b03b1968ded18ce4
Jaehwa Han <drh@wafflestudio.com>
2012-11-16 16:52:39 -0500

    self ip authorized

--- routes/git.js
+++ routes/git.js
@@ -3,7 +3,7 @@ exports.pushed = function(req, res) {
 	var pushInformation = JSON.parse(req.param('payload'));
 	var ip = req.ip;
 
-	var authorized_ips = ["207.97.227.253", "50.57.128.197", "108.171.174.178"];
+	var authorized_ips = ["0.0.0.0", "127.0.0.1", "207.97.227.253", "50.57.128.197", "108.171.174.178"];
 
     if (authorized_ips.indexOf(ip) < 0) { //not authorized
     	res.status(400);
