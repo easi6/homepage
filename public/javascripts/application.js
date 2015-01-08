f1e9588bb7be166e5a03a94f056e27de2e7fa01c
Myunkyu Park <spica@wafflestudio.com>
2015-01-08 19:17:41 +0900

    image 수정, responsive 약간 적용

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
 
   $('#main-nav').scrollspy()
 
+  $('li.item.next').click(function(e) {
+    $('#easi6-modal').modal();
+  });
+
   $('li.item.vox').click(function(e) {
     $('#vox-modal').modal();
   });
